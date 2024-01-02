3cad20590b6523e9bcc298ee202e9368dfef60c9
Suryateja Duvvuri <duvvurisuryateja95@gmail.com>
2024-01-01 21:16:41 -0800

    Initial Commit

---
+++ Meal-Prep-Planner/Meal_Prep_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_Prep_PlannerApp.swift
+//  Meal-Prep-Planner
+//
+//  Created by Suryateja Duvvuri on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Meal_Prep_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
