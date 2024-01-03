8fb81610bac67f2fbc4411365c0e7ed0d4a96193
Suryateja Duvvuri <duvvurisuryateja95@gmail.com>
2024-01-02 22:07:59 -0800

    Add all the navigation views used in order

--- Meal-Prep-Planner/ContentView.swift
+++ Meal-Prep-Planner/ContentView.swift
@@ -9,16 +9,87 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack
+        {
+            Text("Offers buttons")
+            
+            Section
+            {
+                NavigationLink(destination: DurationView(), label: {
+                    Text("Make a meal")
+                })
+            }
+            
+            .padding()
+            .navigationBarTitle("Home Page")
         }
-        .padding()
+        
+        
+       
     }
+}
+
+struct DurationView : View {
+    var body: some View {
+        NavigationView
+        {
+            VStack
+            {
+                Text("Asks for duration for the meals")
+                NavigationLink(destination: MealPlanView(), label: {
+                    Text("5 mins")
+                })
+            }.navigationBarTitle("Duration Page")
+                .navigationBarHidden(true)
+                .padding()
+        }
+       
+    }
+}
+
+
+struct MealPlanView : View {
+    
+    @State private var selectedMeals = ""
+    @State private var meals: [String] = ["Mac and Cheese", "Pasta", "Curd rice"]
+    var body: some View {
+        NavigationStack{
+            
+            NavigationLink(destination: CheckIngredientsView(), label: {
+                ForEach(meals, id: \.self)
+                {   meal in
+                    
+                    Text(meal).frame(width: 100, height: 50).font(.headline).fontDesign(.rounded).background(.gray).foregroundColor(.white)
+
+                }
+            })
+          
+            
+            .pickerStyle(.inline)
+            .navigationBarTitle("Suggested meals")
+        }
+    }
+}
+
+struct CheckIngredientsView : View {
+    var body: some View {
+        NavigationView
+        {
+            NavigationLink(destination: TutorialView())
+            {
+                Text("Asks to check ingredients before tutorial")
+            }
+        }
+    }
+}
+
+struct TutorialView : View {
+    var body: some View {
+        Text("Presents with embed tutorial as a scroll view. Go back to home page")
+    } 
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
 }
